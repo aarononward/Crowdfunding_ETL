445496a6874d0bec302137c3fc300fa506063dbd
aarononward <129822330+aarononward@users.noreply.github.com>
2023-07-13 21:05:10 -0700

    adding SQL schema edits

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -1,12 +1,12 @@
 -- Table: Category
 CREATE TABLE Category (
-    category_id SERIAL PRIMARY KEY,
+    category_id VARCHAR(255) PRIMARY KEY,
     category VARCHAR(255)
 );
 
 -- Table: Subcategory
 CREATE TABLE Subcategory (
-    subcategory_id SERIAL PRIMARY KEY,
+    subcategory_id VARCHAR(255) PRIMARY KEY,
     subcategory VARCHAR(255)
 );
 
@@ -15,17 +15,20 @@ CREATE TABLE Campaign (
     cf_id SERIAL PRIMARY KEY,
     contact_id INTEGER,
     company_name VARCHAR(255),
-    description TEXT,
     goal FLOAT,
     pledged FLOAT,
     outcome VARCHAR(255),
     backers_count INTEGER,
     country VARCHAR(255),
     currency VARCHAR(255),
+	staff_pick BOOLEAN,
+	spotlight BOOLEAN,
+	description TEXT,
     launch_date TIMESTAMP,
     end_date TIMESTAMP,
-    category_id INTEGER,
-    subcategory_id INTEGER,
+    category_id VARCHAR(255),
+    subcategory_id VARCHAR(255),
+	subcategory VARCHAR,
     FOREIGN KEY (category_id) REFERENCES Category(category_id),
     FOREIGN KEY (subcategory_id) REFERENCES Subcategory(subcategory_id)
 );
